6737539e02f9cad6791384bac0f6ec2eeccdd5cf
vden <vden@hmgaz.ru>
2017-02-08 16:29:32 +0500

    Подключение таблицы расширяющей данные пользователя

--- FOadmin/admin.py
+++ FOadmin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from FOadmin.models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    #verbose_name = 'Profile'
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- FOadmin/models.py
+++ FOadmin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, verbose_name='Доп. информайия')
+    fo_patronymic = models.CharField(max_length=50, verbose_name='Отчество')
+
+    class Meta:
+        verbose_name = 'Дополнительная информация'
+        verbose_name_plural = 'Дополнительная информация'
+
