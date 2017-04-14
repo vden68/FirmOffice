c3bfaa5864a108d11e41ae91c8aa68fa3d9ef84f
vden <vden@hmgaz.ru>
2017-04-14 17:18:19 +0500

    12042017

--- FoBusinessProcess/models.py
+++ FoBusinessProcess/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+#Бизнес процесс
+class BusinessProcess(models.Model):
+    fo_business_process_name= models.CharField('Название',max_length=100)
+    fo_line_of_business= models.ForeignKey('LineOfBusiness', on_delete=models.ProtectedError,
+                                           verbose_name='Отрасль производства')
+
+
+#Отрасль производства (линия бизнеса) справочник
+class LineOfBusiness(models.Model):
+    fo_line_of_business_name=  models.CharField('Отрасль производства',max_length=100)
